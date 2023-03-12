8c068ad1feedf4d2c0b56bb505e9236507cdbb5f
SUNMBOLA EMMANUEL ADEDAYO <adedayoemmanuel729@gmail.com>
2023-03-12 16:14:10 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 <h1>Welcome</h1>
 <p>This is the UI of a project we're working on</p>
-<ul>
+<ol>
 <h3>We are working on this project </h3>
 <li>To help gamers buy in game assets with ease</li>
 <li>Because we can 🙂</li>
-</ul>
+</ol>
+<ol>
+<h1>Updates</h1>
+<li>Changed files to php file type since the backend will be done with php</li>
+<li>Seperated the head and nav to avoid repetition</li>
+<li>More to come....</li>
+</ol>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require("head.php");
+?>
+
+
+    <!-- <div class="sidebar">
+
+        <div><a href=""><i class="fa-brands fa-twitter-square"></i></a></div>
+        <div><a href=""><i class="fa-brands fa-instagram-square"></i></a></div>
+        <div class="line"></div>
+    </div> -->
+    <main>
+        <h1>Welcome to <span title="best gaming website">game shop</span> website</h1>
+        <p>We are the fastest , most reliable and trustworthy merchants</p>
+        <p>We give you the best deals for very cheap prices</p>
+    </main>
+</body>
+</html>
